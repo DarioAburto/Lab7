5c985dde3a023e632009001d35e8aad769282f50
DarioAburto <daaburto@ucsd.edu>
2021-05-16 22:11:11 -0700

    done finally hopefully

--- scripts/router.js
+++ scripts/router.js
@@ -1,11 +1,19 @@
 // router.js
-
 export const router = {};
+
+document.querySelector("img").addEventListener("click", function(){
+  router.setState("settings");
+});
+
+document.querySelector("h1").addEventListener("click", function(){
+  router.setState("home");
+});
+
 
 /**
  * Changes the "page" (state) that your SPA app is currently set to
- */
-router.setState = function() {
+*/
+router.setState = function(state) {
   /**
    * - There are three states that your SPA app will have
    *    1. The home page
@@ -35,4 +43,84 @@ router.setState = function() {
    *    1. You may add as many helper functions in this file as you like
    *    2. You may modify the parameters of setState() as much as you like
    */
+
+    
+    if(state == "settings"){
+      history.pushState({page: "settings"}, "settings", "http://127.0.0.1:5500/#settings");
+      document.querySelector("body").className = "settings";
+      document.querySelector("h1").innerHTML = "Settings";
+    }
+    if(state == "home"){
+      history.pushState({page: "home"}, "home", "http://127.0.0.1:5500/");
+      document.querySelector("body").className = "";
+      document.querySelector("h1").innerHTML = "Journal Entries";
+    }
+    if(state == "You like jazz?"){
+      history.pushState({page: "entry1"}, "entry1", "http://127.0.0.1:5500/#entry1");
+      document.querySelector("body").className = "single-entry";
+      document.querySelector("h1").innerHTML = "Entry 1";
+      //let entryPage  = document.querySelector("entry-page");
+      //let journalEntry = document.getElement(state);      
+      //entryPage.entry = journalEntry.entry;
+      //document.querySelector("body").appendChild(entryPage);
+    }
+    if(state == "Run, Forrest! Run!"){
+      history.pushState({page: "entry2"}, "entry2", "http://127.0.0.1:5500/#entry2");
+      document.querySelector("body").className = "single-entry";
+      document.querySelector("h1").innerHTML = "Entry 2";
+    }
+    if( state == "Ogres are like onions"){
+      history.pushState({page: "entry3"}, "entry3", "http://127.0.0.1:5500/#entry3");
+      document.querySelector("body").className = "single-entry";
+      document.querySelector("h1").innerHTML = "Entry 3";
+    }
+    if( state == "You're a wizard, Harry"){
+      history.pushState({page: "entry4"}, "entry4", "http://127.0.0.1:5500/#entry4");
+      document.querySelector("body").className = "single-entry";
+      document.querySelector("h1").innerHTML = "Entry 4";
+    }
+    if ( state == "We know each other! He’s a friend from work!"){
+      history.pushState({page: "entry5"}, "entry5", "http://127.0.0.1:5500/#entry5");
+      document.querySelector("body").className = "single-entry";
+      document.querySelector("h1").innerHTML = "Entry 5";
+    }
+    if ( state == "I am... Iron Man" ){
+      history.pushState({page: "entry6"}, "entry6", "http://127.0.0.1:5500/#entry6");
+      document.querySelector("body").className = "single-entry";
+      document.querySelector("h1").innerHTML = "Entry 6";
+    }
+    if ( state == "Just keep swimming"){
+      history.pushState({page: "entry7"}, "entry7", "http://127.0.0.1:5500/#entry7");
+      document.querySelector("body").className = "single-entry";
+      document.querySelector("h1").innerHTML = "Entry 7";
+    }
+    if(state == "No capes!"){
+      history.pushState({page: "entry8"}, "entry8", "http://127.0.0.1:5500/#entry8");
+      document.querySelector("body").className = "single-entry";
+      document.querySelector("h1").innerHTML = "Entry 8";
+    }
+    if ( state == "Keep moving forward"){
+      history.pushState({page: "entry9"}, "entry9", "http://127.0.0.1:5500/#entry9");
+      document.querySelector("body").className = "single-entry";
+      document.querySelector("h1").innerHTML = "Entry 9";
+    }
+    if( state == "No, I am your father"){
+      history.pushState({page: "entry10"}, "entry1", "http://127.0.0.1:5500/#entry10");
+      document.querySelector("body").className = "single-entry";
+      document.querySelector("h1").innerHTML = "Entry 10";
+    }
+        
+
 }
+/*
+function getEntry(entry){
+
+  let journalEntry = entry;
+  let entryPage = document.querySelector('entry-page');
+
+  entryPage.entry = journalEntry.entry;
+  history.pushState({page: "entry"}, "entry", "http://127.0.0.1:5500/");
+  document.querySelector("body").className = "single-entry"
+
+}
+*/
--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,47 @@
 // script.js
 
 import { router } from './router.js'; // Router imported so you can use it to manipulate your SPA app here
+
+
+
 const setState = router.setState;
 
+window.addEventListener('popstate', (event) => {
+  console.log("location:" + document.location + ", state:" + JSON.stringify(event.state));
+  router.setState(event.state);
+  window.location = document.location;
+});
+
+if('serviceWorker' in navigator) {
+  window.addEventListener('load', function(){
+    navigator.serviceWorker.register('/sw.js').then(function(registration){
+      console.log('ServiceWorker registration successful with scope:' , registration.scope);
+    } , function(err){
+      console.log('ServiceWorker registration failed:' , err);
+    });
+  });
+}
+
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+/*
+window.onpopstate = function(event){
+  router.setState(event.state);
+}
+*/
 // Make sure you register your service worker here too
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -12,6 +51,17 @@ document.addEventListener('DOMContentLoaded', () => {
       entries.forEach(entry => {
         let newPost = document.createElement('journal-entry');
         newPost.entry = entry;
+        newPost.addEventListener("click", function(){
+          let entryPage = document.querySelector("entry-page");
+          if(entryPage.entry.title){
+            document.querySelector("body").removeChild(entryPage);
+            entryPage = document.createElement("entry-page");
+            document.querySelector("body").appendChild(entryPage);
+          }
+          router.setState(entry.title);
+          //document.querySelector("body").className = "single-entry";
+          entryPage.entry = newPost.entry;
+        });
         document.querySelector('main').appendChild(newPost);
       });
     });
